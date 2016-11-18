223ab3e13bc2cc2db926da079e5ae6dc00e59206
Arthur van der Wal <arthur@redpotato.nl>
2016-11-18 13:45:51 +0900

    ad.php removed breakout of sandboxed iframe redirect method (obsolete - not working)

--- ad.php
+++ ad.php
@@ -849,23 +849,6 @@
 
 		adlog($redirectUrl);
 
-		if ($redirectMethod == "breakoutsandboxediframe")
-		{
-			$redirectCode = "var el = document.createElement('script');
-		    				 el.type = 'text/javascript';
-		    				 var code = 'window.top.location = \'$redirectUrl\';';
-
-		    				 try
-		    				 {
-								 el.appendChild(parent.parent.document.createTextNode(code));
-								 parent.parent.document.body.appendChild(el);
-		    				 }
-		    				 catch (e)
-		    				 {
-								 el.text = code;
-								 parent.parent.document.body.appendChild(el);
-		    				 }";
-		}
 		if ($redirectMethod == "windowlocation")
 		{
 			$redirectCode = "window.location = '$redirectUrl' + encodeURIComponent(topDomain) + '&' + location.search.substring(1);";
