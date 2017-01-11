634f24e28b49ada62efb57043a59c4e9f65a2f7d
Arthur van der Wal <arthur@redpotato.nl>
2017-01-11 10:21:46 +0900

    generateadstats.php: Added per parameter support (not finished) adlib.inc: added support for return value for print_r_nice, in line with print_r

--- generateadstats.php
+++ generateadstats.php
@@ -6,12 +6,17 @@
 	{
 		$matches = array();
 
-		$regex = "/$startsWith([^.]*):([^.]*):/";
+		//$startsWith = "CHECK:PARAMETER_ALLOWED:";
+		//$log = "Date|2017-01-10 23:48:58|IP|187.237.231.107|ISP|Telmex|UserAgent|Mozilla/5.0 (Linux; Android 4.0.3; U9200 Build/HuaweiU9200) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19|SERVER Referrer|http://delivery.porn.com/c2hvd19zdGQkJDk2MjUyMA==?ident=8722&id_site=10&id_channel=43&ref=http://es.porn.com/videos/boob-licking&sr=http://www.google.com.mx/search?client=ms-android-huawei&sourceid=chrome-mobile&ie=UTF-8&q=mamando chiches&id_dimension=13&width=300&height=250&id_palette=1&ck=922&uf=-1&vars={\"site_id\":10,\"channel_id\":43}&k=[\"boob licking\"]&m_url=media.porn.com|QueryString|0804664&site_id=10&site_name=Porn.com Mobile&channel_id=43&channel_name=Site Wide Footer&keywords=&ad_id=1013585&v_country=MX&v_language=ES&v_browser=chrome&v_os=android&v_device=Huawei U9200&v_device type=phone&v_carrier=telcel|Message|CHECK:PARAMETER_ALLOWED:site_name:Porn.com Mobile: Parameter site_name with value Porn.com Mobile is allowed.";
+
+		$regex = "/$startsWith(.*):(.*):/";
+
+		//echo $regex;
 
 		preg_match_all($regex, $log, $matches);
 
-		print_r_nice($matches);
-		exit;
+		//print_r_nice($matches);
+		//exit;
 
 		$result = array();
 
@@ -30,7 +35,9 @@
 			$result[$matches[1][$i]][$matches[2][$i]]++;
 		}
 
-		print_r_nice($result);
+		//print_r_nice($result);
+
+		return $result;
 	}
 
 	$ads = getAllAds(__DIR__);
@@ -72,13 +79,13 @@
 					$stats["REFERRER_WHITELIST_BLOCKED"] = substr_count($log, "CHECK:REFERRER_WHITELIST_BLOCKED:");
 					$stats["REFERRER_WHITELIST_ALLOWED"] = substr_count($log, "CHECK:REFERRER_WHITELIST_ALLOWED:");
 					$stats["PARAMETER_BLOCKED"] = substr_count($log, "CHECK:PARAMETER_BLOCKED:");
-					$stats["PARAMETER_ALLOWED"] = substr_count($log, "CHECK:PARAMETER_ALLOWED:");
+					$stats["PARAMETER_ALLOWED"] = getAllNextLevelParametersValues($log, "CHECK:PARAMETER_ALLOWED:"); //substr_count($log, "CHECK:PARAMETER_ALLOWED:");
 					$stats["PARAMETER_MISSING"] = substr_count($log, "CHECK:PARAMETER_MISSING:");
 					$stats["REFERRER_PARAMETER_BLOCKED"] = substr_count($log, "CHECK:REFERRER_PARAMETER_BLOCKED:");
 					$stats["REFERRER_PARAMETER_ALLOWED"] = substr_count($log, "CHECK:REFERRER_PARAMETER_ALLOWED:");
 					$stats["REFERRER_PARAMETER_MISSING"] = substr_count($log, "CHECK:REFERRER_PARAMETER_MISSING:");
 
-					getAllNextLevelParametersValues($log, "CHECK:PARAMETER_ALLOWED:");
+					
 				}
 				elseif (strpos($logFilename, "adlog") !== false)
 				{
@@ -97,7 +104,7 @@
 		$statsPerCampaign[$campaignID] = $stats;
 	}
 
-	echo "<table width=\"100%\" border=\"1\">\n";
+	echo "<table width=\"3800px\" border=\"1\">\n";
 
 	echo "<tr>";
 	echo "<th>Campaign ID</th>\n";
@@ -127,7 +134,7 @@
 		echo "<td>" . $stats["REFERRER_WHITELIST_ALLOWED"] . "</td>";
 
 		echo "<td>" . $stats["PARAMETER_BLOCKED"] . "</td>";
-		echo "<td>" . $stats["PARAMETER_ALLOWED"] . "</td>";
+		echo "<td>" . print_r_nice($stats["PARAMETER_ALLOWED"], true) . "</td>";
 		echo "<td>" . $stats["PARAMETER_MISSING"] . "</td>";
 
 		echo "<td>" . $stats["REFERRER_PARAMETER_BLOCKED"] . "</td>";
