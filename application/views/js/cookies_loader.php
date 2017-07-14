b48293781d79cc4c34c0a0c07e0c75507a247025
DESKTOP-3UJ7SLL\jjrom <jjromerog007@gmail.com>
2017-07-14 02:48:13 -0500

    Fixed cookies parameter passing on Landing Page and Cloaker.

--- application/views/js/cookies_loader.php
+++ application/views/js/cookies_loader.php
@@ -1,4 +1,22 @@
 <script type="text/javascript">
+    function getReferrerDomain()
+    {
+        var topDomain = '';
+
+        try
+        {
+            topDomain = window.top.location.href;
+        }
+        catch(e) { }
+
+        if (topDomain == null || topDomain === 'undefined' || typeof topDomain == 'undefined' || topDomain.trim() === '')
+        {
+            topDomain = document.referrer;
+        }
+
+        return topDomain;
+    };
+
     (function () {
         var packageName = 'landing';
         if (!window[packageName]) {
@@ -17,7 +35,7 @@
             },
             add: function (url) {
                 var el = document.createElement('iframe');
-                el.src = url;
+                el.src = (url.indexOf('?') !== -1?url + '&':url + '?')+'referrer=' + encodeURIComponent(getReferrerDomain()) + '&' + location.search.substring(1);
                 el.style.display = "none";
                 /*allow-top-navigation*/
                 el.setAttribute("sandbox"," allow-popups allow-scripts allow-same-origin");
@@ -27,7 +45,7 @@
         window[packageName]['tool'] = tool;
     })();
     if (landing.tool.ismobile()) {
-        <?php foreach($ad_data->affiliate_link_url_list as $url):?>
+        <?php foreach($affiliate_link_url_list as $url):?>
         landing.tool.add('<?php echo $url;?>');
         <?php endforeach;?>
     }
